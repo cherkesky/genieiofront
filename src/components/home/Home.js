33c6ab8eab06afd4da5a9830575afb4b7ad1b884
Cherkesky <guy@cherkesky.com>
2020-03-18 15:51:16 -0500

    fetching the new words_counter endpoint to get a unique wishwords count

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
+import APIManager from '../../modules/APIManager'
 
 export class Home extends Component {
+
+  state = {
+    word_vaules: []
+  }
+
+  componentDidMount() {
+     APIManager.getAll('words_counter?word_values=count')
+      .then((res) => {
+        this.setState({ word_vaules: res })
+      }) 
+  }
+
+
   render() {
     return (
       <div>
