8f983f7d49fc0700c5a7482600cc04b641fcd618
Cherkesky <guy@cherkesky.com>
2020-03-18 13:58:56 -0500

    initial inbox view

--- src/components/inbox/Inbox.js
+++ src/components/inbox/Inbox.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import APIManager from '../../modules/APIManager';
+import InboxItem from './InboxItem'
 
 export class Inbox extends Component {
+  state={
+    my_grants:[]
+  }
+
+  componentDidMount(){
+    APIManager.getAll("grants?by_wish=id")
+    .then((result) => {
+      this.setState({
+        my_grants: result
+      })
+    });
+  } // closer componentDidMount
+
   render() {
     return (
       <div>
         <h1>INBOX</h1>
+        <InboxItem/>
       </div>
     )
   }
---
+++ src/components/inbox/InboxItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class InboxItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>InboxItem</h1>
+      </div>
+    )
+  }
+}
+
+export default InboxItem
