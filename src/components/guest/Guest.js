37159ff216218b792dbb96cbdba5b334f4059d51
Cherkesky <guy@cherkesky.com>
2020-03-24 16:32:49 -0500

    welcome screen styling and fetching wordcloud words when no auth

--- src/components/guest/Guest.js
+++ src/components/guest/Guest.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import APIManager from '../../modules/APIManager'
+import WordCloudNoAuth from '../guest/WordCloudNoAuth'
 
 export class Guest extends Component {
+  state = {
+    word_values: []
+  }
+
+  componentDidMount() {
+    APIManager.getAllNoAuth('words_counter?word_values=count')
+      .then((res) => {
+        this.setState({ word_values: res })
+      })
+
+  }
   render() {
     return (
-      <div>
-        <h1>Guest</h1>
-      </div>
+      <>
+        <WordCloudNoAuth
+          word_values={this.state.word_values}
+          {...this.props}
+        />
+
+      </>
     )
   }
 }
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
+import Guest from '../guest/Guest'
+
+const styles = {
+  parent: {
+    marginTop: "auto",
+    display: "flex",
+    flexDirection: "column",
+  },
+  button: {
+    marginTop: "auto",
+    display: 'flex',
+    background: "#DC42CC",
+    flexDirection: 'column',
+    justifyContent: 'baseline',
+    padding: 30,
+    height: 30,
+  },
+  front: {
+    zIndex: '1',
+    marginTop: '15%',
+    marginLeft: '35%',
+    position: 'absolute',
+    display: "flex",
+    flexDirection: "column",
+  }
+}
 
 export class Welcome extends Component {
+
   render() {
     return (
       <>
-        <h1>Welcome</h1>
-        <div>
-        <Button variant="contained" color="secondary" style={{ background: '#DC42CC' }} onClick={()=>{ 
-          this.props.history.push("/login")
-        }}>Login </Button>
-        <Button variant="contained" color="default" onClick={()=>{
-          this.props.history.push("/register")
-        }}>Register </Button>
+        <div style={styles.front}>
+          <div>
+            <img alt="logo" src={require("../../assets/logo_nbg.png")} width="380px" height="180px"
+            ></img>
+          </div>
+          <Button variant="contained" color="secondary" onClick={() => {
+            this.props.history.push("/login")
+          }}>Login </Button>
+          <Button variant="contained" color="default" onClick={() => {
+            this.props.history.push("/register")
+          }}>Register </Button>
+          <Button color="default" onClick={() => {
+            this.props.history.push("/guest")
+          }}>Just a guest</Button>
         </div>
-        <div>
-        <Button href="#text-buttons" color="default" onClick={()=>{
-          this.props.history.push("/guest")
-        }}>Just a guest</Button>
-        </div>
+        <Guest />
       </>
     )
   }
